d382a98d4698609120e2d9e137f70ae143b13225
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2018-09-21 17:27:41 -0500

    Added User Module with lazy loading.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -11,5 +11,6 @@ export const appRoutes: Routes = [
   { path: 'events', component: EventsListComponent, resolve: {events: EventsListResolverService } },
   { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivatorService] },
   { path: 'not-found', component: NotFoundComponent },
-  { path: '', redirectTo: '/events', pathMatch: 'full' }
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: 'user', loadChildren: './user/user.module#UserModule' }
 ];
