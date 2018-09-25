51722f8df7c8698d36d9dea67dadef25c780a776
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2018-09-25 14:06:17 -0500

    Added basic validation to login form.

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'af-login',
@@ -7,16 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  userName: string;
-  password: string;
+  private userName: string;
+  private password: string;
+  private mouseoverLogin: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   login(formValues) {
-   console.log(formValues);
+   this.authService.loginUser(formValues.userName,  formValues.password);
+   this.router.navigate(['events']);
   }
 
+  cancel() {
+    this.router.navigate(['events']);
+  }
 }
