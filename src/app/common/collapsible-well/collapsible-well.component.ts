e0dd573e1eeeda9ab7a7d60a05ded950986e112a
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2018-12-04 13:21:47 -0600

    Added multiple content projection to Collapsible Well component.

--- src/app/common/collapsible-well/collapsible-well.component.ts
+++ src/app/common/collapsible-well/collapsible-well.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./collapsible-well.component.css']
 })
 export class CollapsibleWellComponent implements OnInit {
-  @Input() title: string;
   public visible = true;
 
   constructor() { }
