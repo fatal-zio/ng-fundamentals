b5ed81336d8843ead89f37d69077c470b36b9616
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2018-12-06 13:44:16 -0600

    Added code to close the modal dialog if a search item is selected.

--- src/app/common/simple-modal/simple-modal.component.ts
+++ src/app/common/simple-modal/simple-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, Inject } from '@angular/core';
+import { JQ_TOKEN } from '../j-query.service';
 
 @Component({
   selector: 'af-simple-modal',
@@ -8,10 +9,15 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SimpleModalComponent implements OnInit {
   @Input() title: string;
   @Input() elementId: string;
+  @ViewChild('modalcontainer') containerEl: ElementRef;
 
-  constructor() { }
+  constructor(@Inject(JQ_TOKEN) private $: any) { }
 
   ngOnInit() {
+  }
+
+  public closeModal(): void {
+    this.$(this.containerEl.nativeElement).modal('hide');
   }
 
 }
