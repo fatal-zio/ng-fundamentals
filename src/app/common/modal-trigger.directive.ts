482a328ff368c8f174810b7df08816f710fd1369
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2018-12-06 11:24:48 -0600

    Made Modal Dialog more generic.

--- src/app/common/modal-trigger.directive.ts
+++ src/app/common/modal-trigger.directive.ts
@@ -1,11 +1,13 @@
-import { Directive, OnInit, Inject, ElementRef } from '@angular/core';
+import { Directive, OnInit, Inject, ElementRef, Input } from '@angular/core';
 import { JQ_TOKEN } from './j-query.service';
+import { trigger } from '@angular/animations';
 
 @Directive({
   selector: '[afModalTrigger]'
 })
 export class ModalTriggerDirective implements OnInit {
   private el: HTMLElement;
+  @Input('afModalTrigger') modalId: string;
 
   constructor(ref: ElementRef, @Inject(JQ_TOKEN) private $: any) {
     this.el = ref.nativeElement;
@@ -13,7 +15,7 @@ export class ModalTriggerDirective implements OnInit {
 
   ngOnInit(): void {
     this.el.addEventListener('click', e => {
-      this.$('#simple-modal').modal({});
+      this.$(`#${this.modalId}`).modal({});
     });
   }
 
