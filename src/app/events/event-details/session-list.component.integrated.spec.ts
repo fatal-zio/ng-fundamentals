2a95892a739c32c177a61d9d6545006a7661141a
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2018-12-10 17:12:07 -0600

    Fixed nesting issue that was preventing SessionList Integration tests from firing.

--- src/app/events/event-details/session-list.component.integrated.spec.ts
+++ src/app/events/event-details/session-list.component.integrated.spec.ts
@@ -29,31 +29,30 @@ describe('SessionListComponentIntegrated', () => {
       ],
       schemas: []
     });
-
-    beforeEach(() => {
-      fixture = TestBed.createComponent(SessionListComponent);
-      component = fixture.componentInstance;
-      debugEl = fixture.debugElement;
-      element = fixture.nativeElement;
-    });
-
-    describe('initial display', () => {
-      it('should have the correct session title', () => {
-        component.sessions = [{
-          id: 3, name: 'Session 1', presenter: 'Joe', duration: 1, level: 'beginner',
-          abstract: 'abstract', voters: ['john', 'bob']
-        }];
-
-        component.filterBy = 'all';
-        component.sortBy = 'name';
-        component.eventId = 4;
-
-        component.ngOnChanges();
-        fixture.detectChanges();
-
-        expect(element.querySelector('[well-title]').textContent).toContain('Session 1S');
-      });
-    });
-
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SessionListComponent);
+    component = fixture.componentInstance;
+    debugEl = fixture.debugElement;
+    element = fixture.nativeElement;
+  });
+
+  describe('initial display', () => {
+    it('should have the correct session title', () => {
+      component.sessions = [{
+        id: 3, name: 'Session 1', presenter: 'Joe', duration: 1, level: 'beginner',
+        abstract: 'abstract', voters: ['john', 'bob']
+      }];
+
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.eventId = 4;
+
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expect(element.querySelector('[well-title]').textContent).toContain('Session 1S');
+    });
+  });
 });
